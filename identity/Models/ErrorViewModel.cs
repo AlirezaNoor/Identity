4bc7982487e57eb720232210a25e5c2a008878c1
AlirezaNoor <alirezang7575@gmail.com>
2022-03-25 10:34:47 +0430

    made project

---
+++ identity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
